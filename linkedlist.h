f28baaa6dc2c2f565a8cbda7e02c682c2ad5b382
Khalil Ahmed <mkhalila@outlook.com>
2016-10-16 01:16:22 +0100

    Created constructor for linkedlist class

--- linkedlist.h
+++ linkedlist.h
@@ -6,10 +6,15 @@
 // TODO your code goes here:
 template<typename T>
 class LinkedList {
+
 private:
 	Node<T> * head;
 	Node<T> * tail;
 	int size;
+
+public:
+	LinkedList() 
+	: head(nullptr), tail(nullptr), size(0) {}
 };
 
 
